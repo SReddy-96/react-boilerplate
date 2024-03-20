279ef439407b43e52fe9d09e206e43875848c7d2
Steven Reddy <steveng.reddy@gmail.com>
2024-03-20 18:22:49 +0000

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+import WebServer from './web.server'
+
+let webServer = new WebServer();
+
+webServer.start()
+    .then(() => {
+        console.log('Web Sever started!')
+    })
+    .catch(err => {
+        console.error(err)
+        console.error('Failed to start web server')
+    });
+
